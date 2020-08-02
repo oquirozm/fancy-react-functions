2a9a6621e22705a376f50ec259a19e8e159684aa
Omar Quiroz <omar@pragmateam.dev>
2020-08-02 02:32:05 +0200

    add preloadableLazy, which adds a method to preload a lazily loaded component.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+import React from "react";
+
 function preloadImage(src) {
   return new Promise(resolve => {
     const img = document.createElement("img");
     img.onload = () => resolve(src);
     img.src = src;
   })
+}
+
+function preloadableLazy(dynamicImport) {
+  let promise;
+
+  function load() {
+    if (!promise) {
+      promise = dynamicImport();
+    }
+    return promise;
+  }
+
+  const Comp = React.lazy(load);
+  Comp.preload = load;
+  return Comp;
 }
